d51c3d4dd4f31120563760bcd932f58d86b60fec
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-19 16:39:39 +0530

    :heavy_plus_sign: Add puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import algoliasearch from "algoliasearch";
 import { config, cosmicSync } from "@anandchowdhary/cosmic";
-cosmicSync("algoliacrawl");
+import {} from "puppeteer";
 
+cosmicSync("algoliacrawl");
 const client = algoliasearch(config("appId"), config("apiKey"));
 const index = client.initIndex(config("index"));
 
 /** Index objects in Algolia search */
 export const indexObjects = async (objects: Readonly<Record<string, any>>[]) => index.saveObjects(objects);
+
+export const getUrls = async () => {};
+getUrls();
