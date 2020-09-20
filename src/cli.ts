df21e415cf7f0f0e1814200d7eb6d19034fe4e2a
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-20 14:21:14 +0530

    :sparkles: Add CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,11 @@
+#!/usr/bin/env node
 import { argv } from "yargs";
 import { algoliaCrawl } from "./crawler";
 import { generateSitemap } from "./sitemap";
+
+if (argv._[0] === "sitemap") {
+  if (typeof argv._[1] === "string") generateSitemap(argv._[1]);
+  else throw new Error("Provide a sitemap file name");
+} else if (argv._[0] === "crawl") {
+  algoliaCrawl();
+}
